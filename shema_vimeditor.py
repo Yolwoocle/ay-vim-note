0f391b8537db07aac09d98aff0eca55d485de3a3
ARKANYOTA <38374187+ARKANYOTA@users.noreply.github.com>
2021-08-14 01:07:34 +0200

    add shema_vimeditor

---
+++ shema_vimeditor.py
@@ -0,0 +1,86 @@
+                                |                     |                     |
+ Matiere                        | Cours               | Toc                 |
+                                |                     |                     |
+  - Anglais                     |  - 1_Grammaire      |  - 1.1 OK           |
+  - Mathématique                |    - 1.1.0_How Works     - 1.2 Gramaire   |
+  - Français**                  |    - 1.2.0_Francais    - 2.1 Ls           |
+                                |  - 2_Verbes         |                     |
+                                |    -                |---------------------|
+                                |                     |                     |
+                                |                     | Infos               |
+                                |                     |                     |
+                                |                     |  Mots 11023         |
+                                |                     |  Char 10203032      |
+                                |                     |  Lign 1123          |
+                                |                     |  Type File          |
+                                |                     |---------------------|
+                                |                     |                     |
+                                |                     | Attribute f         |
+                                |                     |                     |
+                                |                     |  -2- Exporter       |
+                                |                     |      Nouveau        
+                                |                     |      Nouveau chapitre
+---MCTIA--Nouveau-----|                     |      Nouveau Document
+   ↑       ↑          |                     |      Nouvelle partie|
+   MCTIA   Si appuie sur N                  |                     |
+   aoont              |                     |                     |
+   tucft              |                     |                     |
+   ir or              |                     |                     |
+   es si              |                     |                     |
+   r   b              |                     |                     |
+   e   u              |                     |                     |
+       t              |                     |                     |
+       e              |                     |                     |
+                      |                     |                     |
+XMCTIAX--Nouveau--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+Last acctions         |                     |                     |
+                      |                     |                     |
+                      |                     |                     |
+
+                                        |
+ Matiere                                |
+                                        |
+  - X                                   |
+  - C                                   |
+  - V                                   |
+  - B                                   |
+  - N                                   |
+  -                                     |
+  -                                     |
+                                        |
+                                        |
+                                        |
+                                        |
+                                        |
+
+
+
+M → Show Matiere
+C → Show Cours 
+T → Show Toc
+I → Show Infos
+A → Show Attributes
+V → Show Visual 
+
+s = activate math
+d = activate dark
+f = activate css
+
+E = Exporter# TODO
+Backspace = Delete# TODO
+N = Nouveau Mode  # TODO
+N+C = Nouveau Chapitre# TODO
+N+D = Nouveau Document# TODO
+N+P = Nouvelle Partie# TODO
+
+hjkl ←↓↑→ Enter = Navigate# TODO
+
+q = exit
+
+Todo:
+    Finir
+        Box
+            Acctions
+        TODO Things
+    Eporter tout en html et le faire visible pour le mettre sur pages 
+    Faire une meilleur verison de vim
